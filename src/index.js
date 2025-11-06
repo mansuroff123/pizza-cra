6090c6af68f13ccdb28e38ab439c3e7df56fd794
mansuroff123 <mansuroff145@gmail.com>
2025-11-07 04:07:30 +0500

    added time logical section

--- src/index.js
+++ src/index.js
@@ -70,6 +70,14 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen)
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're closed");
   return (
     <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
   );
